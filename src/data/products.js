cf3c9536ef3e20b60780a8830b599fe3596cf9e6
Worgzz333r <golubeckola@gmail.com>
2026-05-29 16:20:43 +0300

    Structure created

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "Yumbox 21 сет",
+    weight: "1500 г",
+    price: 799,
+    image: "/images/product1.jpg"
+  },
+  {
+    id: 2,
+    name: "Yumbox 21 сет",
+    weight: "1500 г",
+    price: 799,
+    image: "/images/product2.jpg"
+  },
+  {
+    id: 3,
+    name: "Yumbox 21 сет",
+    weight: "1500 г",
+    price: 799,
+    image: "/images/product3.jpg"
+  },
+  {
+    id: 4,
+    name: "Yumbox 21 сет",
+    weight: "1500 г",
+    price: 799,
+    image: "/images/product4.jpg"
+  }
+]
+
+export default products
