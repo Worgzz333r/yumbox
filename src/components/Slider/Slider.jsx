70df3fa811d5011271947446315068afc01ec37e
Worgzz333r <golubeckola@gmail.com>
2026-05-30 18:32:46 +0300

    feat: готовий слайдер

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,6 +7,16 @@ import '../../index.scss'
 import './Slider.scss'
 import arrowLeft from '../../assets/arrow-left.svg'
 import arrowRight from '../../assets/arrow-right.svg'
+import set from '../../assets/set.png'
+import patch from '../../assets/patch.png'
+import vector from '../../assets/vector.png'
+import text from '../../assets/text.png'
+
+const slides = [
+  { title: 'сет 21', price: '934 грн' },
+  { title: 'сет 22', price: '1753 грн' },
+  { title: 'сет 23', price: '1275 грн' },
+]
 
 function Slider() {
     return (
@@ -31,13 +41,43 @@ function Slider() {
                     slidesPerView={1}
                 >
                     <SwiperSlide>
-                        <img src="https://placehold.co/1275x500" alt="Слайд 1" />
+                        <img className="slide-patch" src={patch} alt="patch image" />
+                        <img className="slide-set" src={set} alt="set image" />
+
+                        <div className="text-wrapper">
+                            <img className="slide-vector" src={vector} alt="vector image" />
+                            <img className="slide-text" src={text} alt="text image" />
+                        </div>
+                        <div className="slide_info">
+                            <span>{slides[0].title}</span>
+                            <span className="slide-price">{slides[0].price}</span>
+                        </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src="https://placehold.co/1275x500" alt="Слайд 1" />
+                        <img className="slide-patch" src={patch} alt="patch image" />
+                        <img className="slide-set" src={set} alt="set image" />
+
+                        <div className="text-wrapper">
+                            <img className="slide-vector" src={vector} alt="vector image" />
+                            <img className="slide-text" src={text} alt="text image" />
+                        </div>
+                        <div className="slide_info">
+                            <span>{slides[1].title}</span>
+                            <span className="slide-price">{slides[1].price}</span>
+                        </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src="https://placehold.co/1275x500" alt="Слайд 1" />
+                        <img className="slide-patch" src={patch} alt="patch image" />
+                        <img className="slide-set" src={set} alt="set image" />
+
+                        <div className="text-wrapper">
+                            <img className="slide-vector" src={vector} alt="vector image" />
+                            <img className="slide-text" src={text} alt="text image" />
+                        </div>
+                        <div className="slide_info">
+                            <span>{slides[2].title}</span>
+                            <span className="slide-price">{slides[2].price}</span>
+                        </div>
                     </SwiperSlide>
 
 
