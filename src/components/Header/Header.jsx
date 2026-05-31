7d428269cbc29fca94854a5b4e2764c949b21ce5
Worgzz333r <golubeckola@gmail.com>
2026-05-31 16:34:23 +0300

    feat: Мобільний хедер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 import logo from '../../assets/yumbox-logo.svg'
+import burger from '../../assets/burger.svg'
+import close from '../../assets/close.svg'
 import styles from './Header.module.scss'
 import '../../index.scss'
 
@@ -6,30 +9,62 @@ const cartItemsCount = 2;
 const cartItemsPrice = 2568;
 
 function Header() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
     return (
-        <div className={'container'}>
-            <div className={styles['header-wrapper']}>
-                <div className={styles['logo-wrapper']}>
-                    <img src={logo} alt="logo" />
+        <header>
+            <div className={'container'}>
+                {/* DESKTOP */}
+                <div className={styles['header-wrapper']}>
+                    <div className={styles['logo-wrapper']}>
+                        <img src={logo} alt="logo" />
+                    </div>
+
+                    <nav className={styles['header-nav']}>
+                        <a href="#">Каталог</a>
+                        <a href="#">Кейтеринг</a>
+                        <a href="#">Про нас</a>
+                        <a href="#">Контакти</a>
+                    </nav>
+
+                    <div className={styles['cart-wrapper']}>
+                        <div className={styles['cart-items-count']}>
+                            {cartItemsCount}
+                        </div>
+                        <div className={styles['cart-items-price']}>
+                            {cartItemsPrice.toLocaleString('uk-UA')} грн
+                        </div>
+                    </div>
                 </div>
 
-                <nav className={styles['header-nav']}>
-                    <a href="#">Каталог</a>
-                    <a href="#">Кейтеринг</a>
-                    <a href="#">Про нас</a>
-                    <a href="#">Контакти</a>
-                </nav>
-
-                <div className={styles['cart-wrapper']}>
-                    <div className={styles['cart-items-count']}>
-                        {cartItemsCount}
+                {/* MOBILE */}
+                <div className={styles['header-mobile']}>
+                    <div className={styles['logo-wrapper']}>
+                        <img src={logo} alt="logo" />
                     </div>
-                    <div className={styles['cart-items-price']}>
-                        {cartItemsPrice.toLocaleString('uk-UA')} грн
-                    </div>
+                    <button className={styles['burger']} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        Меню {isMenuOpen ? <><img src={close}/></> : <><img src={burger}/></>}
+                    </button>
                 </div>
             </div>
-        </div>
+            {/* MOBILE MENU */}
+            {isMenuOpen && (
+                <div className={styles['mobile-menu']}>
+                    <div className={styles['cart-wrapper']}>
+                        <div className={styles['cart-items-count']}>{cartItemsCount}</div>
+                        <div className={styles['cart-items-price']}>{cartItemsPrice.toLocaleString('uk-UA')} грн</div>
+                    </div>
+                    <nav>
+                        <a href="#">Каталог</a>
+                        <a href="#">Кейтеринг</a>
+                        <a href="#">Про нас</a>
+                        <a href="#">Контакти</a>
+                    </nav>
+                </div>
+            )}
+
+        </header>
     )
 }
 
